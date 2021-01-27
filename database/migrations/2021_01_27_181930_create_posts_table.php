ac17cc9a70e7cf735aba4dd73092a3db3d4714e9
damianArgote <argotedamian@gmail.com>
2021-01-27 15:54:32 -0300

    Feature Model and Migration Post

---
+++ database/migrations/2021_01_27_181930_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('image');
+            $table->string('content');
+        });
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('categoryId');
+        
+            $table->foreign('categoryId')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
